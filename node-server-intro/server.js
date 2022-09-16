f5e932572664bd55151ce3e1468e2ae83254d7f7
Joe-Lindie <Josephlindie@hotmail.co.uk>
2022-09-16 14:05:25 +0100

    testing the server

--- node-server-intro/server.js
+++ node-server-intro/server.js
@@ -4,5 +4,5 @@ const server = express();
 module.exports = server;
 
 server.get("/", (request, response) => {
-  response.send("NOW CHANGE TO THIS");
+  response.send("NOW CHANGE TO THIS, just to check");
 });
