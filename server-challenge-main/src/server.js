ff7e2da8a28a1e0bdb79a2413b07b8f114de4731
Joe-Lindie <Josephlindie@hotmail.co.uk>
2022-09-19 12:25:56 +0100

    challenge 3

--- server-challenge-main/src/server.js
+++ server-challenge-main/src/server.js
@@ -32,27 +32,75 @@ server.get("/", (request, response) => {
 
 // How do search parementer work? request.query.hex;
 
+// server.get("/colour", (request, response) => {
+//   let hex = request.query.hex;
+
+//   if (hex === undefined) {
+//     hex = "0000FF"; //hex for white #ffffff
+//   }
+
+//   response.send(
+//     ` <!doctype html>
+//         <html>
+//           <head>
+//             <meta charset="utf-8">
+//             <title>Home</title>
+//           </head>
+//           <body>
+//           <style>
+//             body { background-color: #${hex}}
+//           </style>
+//           <h1>Hello Express</h1>
+//           </body>
+//         </html>
+//   `
+//   );
+// });
+
+//CHALLENGE 3
+
+// Edit your GET /colour route to include a form in the HTML response. This form should include an input for the user to type in a hex code.
+
+//When the form is submitted a GET request will be sent to the same route, which will change the background of the page to whatever the user entered.
+
+//Comment out challenge 2
+// Re write challenger 2
+// Add form tags to HTML response
+// Add input tag inside form
+// add get request to form
+
 server.get("/colour", (request, response) => {
   let hex = request.query.hex;
 
   if (hex === undefined) {
-    hex = "0000FF"; //hex for white #ffffff
+    hex = "ffffff"; //hex for white #ffffff
   }
 
-  response.send(
-    ` <!doctype html>
-        <html>
-          <head>
-            <meta charset="utf-8">
-            <title>Home</title>
-          </head>
-          <body>
-          <style>
+  response.send(`
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+
+       <style>
             body { background-color: #${hex}}
-          </style>
-          <h1>Hello Express</h1>
-          </body>
-        </html>
-  `
-  );
+       </style>
+
+    <h2>Enter HEX to change background colour</h2>
+
+    <form action="/colour" method="GET">
+    <label for="colour">colour</label>
+      <input id="colour" type="text" name="hex" />
+      <button>HEX</button>
+    </form>
+
+  </body>
+</html>
+
+ `);
 });
