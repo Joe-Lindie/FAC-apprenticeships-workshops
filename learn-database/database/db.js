852d564d7524ba0df088b1668d81e862661054f0
Joe-Lindie <Josephlindie@hotmail.co.uk>
2022-09-25 12:41:54 +0100

    Using our database ISSUE

--- learn-database/database/db.js
+++ learn-database/database/db.js
@@ -1,36 +1,48 @@
+// const { readFileSync } = require("node:fs");
+// const { join } = require("node:path");
+// const database = require("better-sqlite3");
+
+// const db = new database(process.env.DB_FILE);
+// //console.log(db);
+
+// const schemaPath = join("database", "schema.sql");
+// const schema = readFileSync(schemaPath, "utf-8");
+// db.exec(schema);
+
+// /////////////////////////
+
+// // Examples of selecting contect from table
+
+// //////////////////////////
+
+// // const select_date = db.prepare("SELECT DATE()");
+// // const result = select_date.get();
+// // console.log(result);
+
+// // const select_table = db.prepare("SELECT name FROM sqlite_schema");
+// // const result = select_table.all();
+// // console.log(result);
+
+// /////////////////////////
+
+// // Examples of selecting contect from table
+
+// /////////////////////////
+
+// //.run() when you don’t need a result
+// //.get() when you expect a single row
+// //.all() when you want to get all rows matching the query.
+
+// module.exports = db;
+
 const { readFileSync } = require("node:fs");
 const { join } = require("node:path");
-const database = require("better-sqlite3");
+const Database = require("better-sqlite3");
 
-const db = new database(process.env.DB_FILE);
-console.log(db);
-
-/////////////////////////
-
-// Examples of selecting contect from table
-
-//////////////////////////
-
-// const select_date = db.prepare("SELECT DATE()");
-// const result = select_date.get();
-// console.log(result);
-
-// const select_table = db.prepare("SELECT name FROM sqlite_schema");
-// const result = select_table.all();
-// console.log(result);
-
-/////////////////////////
-
-// Examples of selecting contect from table
-
-/////////////////////////
+const db = new Database(process.env.DB_FILE);
 
 const schemaPath = join("database", "schema.sql");
 const schema = readFileSync(schemaPath, "utf-8");
 db.exec(schema);
-
-//.run() when you don’t need a result
-//.get() when you expect a single row
-//.all() when you want to get all rows matching the query.
 
 module.exports = db;
---
+++ learn-database/index.js
@@ -0,0 +1,9 @@
+const db = require("./database/db.js");
+const { createTask } = require("./model/tasks.js");
+
+// createTask("Eat a banana");
+// const tasks = db.prepare("SELECT * FROM tasks").all();
+// console.log(tasks);
+
+const result = createTask("Send mum flowers");
+console.log(result);
