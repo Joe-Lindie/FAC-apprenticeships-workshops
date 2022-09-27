d7c2876d63e501aedef3ea91f352b3df4f22060a
Joe-Lindie <Josephlindie@hotmail.co.uk>
2022-09-27 11:55:41 +0100

    challenge 5

--- database-challenge-main/src/model/products.js
+++ database-challenge-main/src/model/products.js
@@ -6,20 +6,20 @@ const db = require("../database/db.js");
 
 //////////////////////////
 
-const retrieve_products = db.prepare(/*sql*/ `
-  
-SELECT 
-      id, 
-      name, 
-      quantity_per_unit, 
-      unit_price, 
-      units_in_stock,
-      units_on_order FROM products 
-`);
+// const retrieve_products = db.prepare(/*sql*/ `
 
-function listProducts() {
-  return retrieve_products.all();
-}
+// SELECT
+//       id,
+//       name,
+//       quantity_per_unit,
+//       unit_price,
+//       units_in_stock,
+//       units_on_order FROM products
+// `);
+
+// function listProducts() {
+//   return retrieve_products.all();
+// }
 
 //EXPORT STATMENT AT BOTTOM ↓↓↓↓↓↓
 
@@ -43,13 +43,15 @@ const search_products = db.prepare(/*sql*/ `
 // ? is a placeholder for a value
 // ("%" + search_string + "%") Finds any values that have 'search_string' in any position
 
+// Is
+
 function searchProducts(search_string) {
-  return search_products.all("%" + search_string + "%");
+  return search_products.all("_" + search_string);
 }
 
 //////////////////////////
 
-//CHALLENGE 3: SEARCH PRODUCTS
+//CHALLENGE 3:  Get specific product
 
 //////////////////////////
 
@@ -60,8 +62,44 @@ function getProduct(id) {
   return matching_product.get(id);
 }
 
-// if console.log(getProduct(1)); //  { id: 1 }
-// output: {name: 'chai'}
-// expected output {id: 1, name: 'chai'}
+//console.log(getProduct(4));
 
 module.exports = { listProducts, searchProducts, getProduct };
+
+// if console.log(getProduct(1)); //  { id: 1 }
+// output: {name: 'chai'}
+// expected output [{id: 1, name: 'chai'}]
+
+//////////////////////////
+
+//CHALLENGE 4:  ADD CATEGORY INFO
+
+//////////////////////////
+
+//////////////////////////
+
+//CHALLENGE 5:  CALCULATE STOCK VALUE
+
+//////////////////////////
+
+const calculate_stock = db.prepare(/*sql*/ `
+  
+SELECT 
+      id, 
+      name, 
+      quantity_per_unit, 
+      unit_price, 
+      units_in_stock,
+      unit_price * units_in_stock AS stock_value,
+      units_on_order FROM products 
+`);
+
+function listProducts() {
+  return calculate_stock.all();
+}
+
+//////////////////////////
+
+//CHALLENGE 6:  FORMAT CURRENCIES
+
+//////////////////////////
