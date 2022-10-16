453cbfab9b24f6359980630af4f951059e074f12
Joe-Lindie <Josephlindie@hotmail.co.uk>
2022-10-16 12:49:30 +0100

    challenge 1

---
+++ react-challenge-main/challenge/App.jsx
@@ -0,0 +1,11 @@
+import Greeting from "./Greeting";
+
+function App() {
+  return (
+    <main>
+      <Greeting name="Joe" />
+    </main>
+  );
+}
+
+export default App;
---
+++ react-challenge-main/challenge/Greeting.jsx
@@ -0,0 +1,7 @@
+//CHALLENGE 1: CREATING COMPONENTS
+
+function Greeting(prop) {
+  return <p>Hello {prop.name}</p>;
+}
+
+export default Greeting;
